2eb5ad25c5ba4f6d4ed4d42792eca44dc9159bf1
Md. Tanzid Hossain <mhossain183055@bscse.uiu.ac.bd>
2022-05-12 12:59:06 +0600

    X changed some variable name and commiting it in may 12

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -2,20 +2,20 @@ class FindLargestShape
 {
     public static void main(String arg[])
     {
-        Rectangle r = new Rectangle(10, 4);
-        Square s = new Square(7);
-        Circle c = new Circle(3.5);
+        Rectangle rec = new Rectangle(10, 4);
+        Square sqre = new Square(7);
+        Circle crcl = new Circle(3.5);
         
-        System.out.println("Rectangle Area : " + r.getArea());
-        System.out.println("Square Area : " + s.getArea());
-        System.out.println("Circle Area : " + c.getArea());
+        System.out.println("Rectangle Area : " + rec.getArea());
+        System.out.println("Square Area : " + sqre.getArea());
+        System.out.println("Circle Area : " + crcl.getArea());
         System.out.println();
         
-        if ((r.getArea() > c.getArea()) && (r.getArea() > s.getArea()))
+        if ((rec.getArea() > crcl.getArea()) && (rec.getArea() > sqre.getArea()))
         {
             System.out.println("Rectangle has the largest area.");
         }
-        else if( s.getArea() > c.getArea() )
+        else if( sqre.getArea() > crcl.getArea() )
         {
             System.out.println("Square has the largest area.");
         }
